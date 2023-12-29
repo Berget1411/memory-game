c6855de9c00b199a096b2da3d8e7444e2ef89f98
Berget1411 <ludvig.benjaminbergstrom@gmail.com>
2023-12-29 11:33:01 +0100

    add: create footer component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import '../styles/footer.scss';
+
+function Footer() {
+  return (
+    <footer>
+      <p>
+        Developed by <a href='https://github.com/Berget1411'>Berget</a>
+      </p>
+    </footer>
+  );
+}
+
+export default Footer;
